71727cee257c44eca390536bef83db04893a6523
Arthur Konrath <konarthur@gmail.com>
2021-03-29 21:04:21 -0300

    +++

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,6 +44,7 @@ EOF
 # - Sync using our dedicated profile and suppress verbose messages.
 #   All other flags are optional via the `args:` directive.
 sh -c "cp .env.${ENVTYPE} .env" \
+&& sh -c "yarn install --production --frozen-lockfile" \
 && sh -c "yarn build" \
 && sh -c "aws s3 sync ${SOURCE_DIR:-public} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile react-deploy-to-s3-action \
