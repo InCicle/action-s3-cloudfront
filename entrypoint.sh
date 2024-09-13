7462f3abcf4d026fcb2d1c711667b2cbf8afc2f0
Kenedy Morais <kennedy.moraes3@gmail.com>
2024-09-12 21:24:17 -0300

    build adjustment

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,31 +44,17 @@ EOF
 # - Build react bundle
 # - Sync using our dedicated profile and suppress verbose messages.
 #   All other flags are optional via the `args:` directive.
-# sh -c "cp .env.${ENVTYPE} .env" \
-# && sh -c "yarn" \
-# && sh -c "CI='' yarn build" \
-# && sh -c "aws s3 sync ${SOURCE_DIR:-public} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
-#               --profile react-deploy-to-s3-action \
-#               --no-progress \
-#               --metadata-directive REPLACE \
-#               --expires ${TIME} \
-#               --acl public-read \
-#               --cache-control ${CACHEAGE} \
-#               ${ENDPOINT_APPEND} $*"
-
 sh -c "cp .env.${ENVTYPE} .env" \
 && sh -c "yarn" \
 && sh -c "CI='' yarn build" \
-
-aws s3 sync ${SOURCE_DIR:-public} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
-  --profile react-deploy-to-s3-action \
-  --no-progress \
-  --metadata-directive REPLACE \
-  --expires ${TIME} \
-  --acl public-read \
-  --cache-control ${CACHEAGE} \
-  ${ENDPOINT_APPEND} $*
-
+&& sh -c "aws s3 sync ${SOURCE_DIR:-public} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
+              --profile react-deploy-to-s3-action \
+              --no-progress \
+              --metadata-directive REPLACE \
+              --expires ${TIME} \
+              --acl public-read \
+              --cache-control ${CACHEAGE} \
+              ${ENDPOINT_APPEND} $*"
 
 SUCCESS=$?
 
